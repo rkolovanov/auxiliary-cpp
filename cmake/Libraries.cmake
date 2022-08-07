8d0236234261854d08f92852991fd91bf6a5b355
rkolovanov <rodion.kolovanov@inbox.ru>
2022-08-07 22:02:47 +0300

    Обновление сборки cmake-build-system

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -1,24 +1,24 @@
-include(GNUInstallDirs)
-include(cmake/Pack.cmake)
+# Project header files
+include_directories(${PROJECT_SOURCE_DIR})
 
-# Boost
+# cmake-build-system
+find_package(cmake-build-system 0.0.2 REQUIRED)
+
+# Boost library
 set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 
-find_package(Boost COMPONENTS chrono date_time thread REQUIRED)
+find_package(Boost 1.74.0 COMPONENTS chrono thread REQUIRED)
 
 include_directories(${Boost_INCLUDE_DIRS})
 
-# Qt 6
+# Qt 6 library
 set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
 set(CMAKE_AUTOUIC ON)
 
-find_package(Qt6 REQUIRED COMPONENTS Core Widgets)
+find_package(Qt6 6.2.4 COMPONENTS Core Widgets REQUIRED)
 
 include_directories(${Qt6Core_INCLUDE_DIRS})
 include_directories(${Qt6Widgets_INCLUDE_DIRS})
-
-# CMakeBuildSystem
-find_package(cmake-build-system)
--- cmake/Pack.cmake
+++ cmake/Pack.cmake
@@ -1,1 +1,13 @@
-set(CPACK_DEB_COMPONENT_INSTALL ON)
+# Add package information
+set(CPACK_PACKAGE_RELEASE 1)
+set(CPACK_DEBIAN_BUILDREQUIRES "cmake >= 3.16, libboost-all-dev >= 1.74.0, qt6-base-dev >= 6.2.4")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-all-dev (>= 1.74.0), qt6-base-dev (>= 6.2.4)")
+
+# Include cmake-build-system Pack.cmake
+if(DEFINED CMAKE_BUILD_SYSTEM_DIR)
+  include(${CMAKE_BUILD_SYSTEM_DIR}/Pack.cmake)
+endif()
+
+# Dependencies for cmake-build-system pack targets
+add_dependencies(pack-deb ${PROJECT_NAME})
+add_dependencies(pack-rpm ${PROJECT_NAME})
