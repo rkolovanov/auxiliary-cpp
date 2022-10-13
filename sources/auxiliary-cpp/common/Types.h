60252a22907bbefd0e4ddd12a8d5725b26f90f20
rkolovanov <rodion.kolovanov@inbox.ru>
2022-10-13 19:00:56 +0300

    Real types

--- sources/auxiliary-cpp/common/Types.h
+++ sources/auxiliary-cpp/common/Types.h
@@ -14,19 +14,15 @@
 
 namespace Auxiliary::Common::Types
 {
-  using Byte = uint8_t;
-
   using Size8 = uint8_t;
   using Size16 = uint16_t;
   using Size32 = uint32_t;
   using Size64 = uint64_t;
-  using Size = Size64;
 
   using Index8 = uint8_t;
   using Index16 = uint16_t;
   using Index32 = uint32_t;
   using Index64 = uint64_t;
-  using Index = Index64;
 
   using Int8 = int8_t;
   using Int16 = int16_t;
@@ -34,7 +30,14 @@ namespace Auxiliary::Common::Types
   using Int64 = int64_t;
   using Int = Int32;
 
-  using Real = long double;
+  using Real4 = float;
+  using Real8 = double;
+  using Real16 = long double;
+
+  using Byte = Int8;
+  using Size = Size64;
+  using Index = Index64;
+  using Real = Real16;
 } // namespace Auxiliary::Common::Types
 
 #endif // SOURCES_AUXILIARY_CPP_COMMON_TYPES_H
